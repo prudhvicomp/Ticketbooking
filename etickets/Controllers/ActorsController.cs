8ed1e120fdb90efeb57a95164f0c985b5734e67f
prudhvicomp <pruthviteja.gokula@gmail.com>
2022-06-22 10:44:47 +0530

    updated

---
+++ etickets/Controllers/ActorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using etickets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace etickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IActionResult> Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+
+
+    }
+}
---
+++ etickets/Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using etickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace etickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
